5cc4a12ee4435568e86ef4bb41ce3cbd2e423f4c
AleLor <alejandrolorefice1@gmail.com>
2018-09-16 20:24:39 -0300

    Update scrip.py

--- PycharmProjects/ITCHALL_ARGENTINA/scrip.py
+++ PycharmProjects/ITCHALL_ARGENTINA/scrip.py
@@ -1,7 +1,7 @@
-adn = "A"
-n = 20
-orden = {ord('A'): 'AL', ord('L'): 'PACA', ord('P'): 'CP', ord('C'): 'PC'}
-for x in range(n):
-    adn = adn.translate(orden)
-
-print(adn)
+def alpaca(n):
+    adn = "A"
+    orden = {ord('A'): 'AL', ord('L'): 'PACA', ord('P'): 'CP', ord('C'): 'PC'}
+    for x in range(n):
+        adn = adn.translate(orden)
+    print("Cadena completa:",adn)
+    print("Cantidad de ALPACA:", adn.count("ALPACA"))
